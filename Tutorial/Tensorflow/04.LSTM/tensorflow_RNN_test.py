c07100ae2946510899571939ec6329c94843cbaa
hyunyoung2 <hyun02.engineer@gmail.com>
2020-03-09 19:22:45 +0900

    Bidirectional GRU code

--- Tutorial/Tensorflow/04.LSTM/tensorflow_RNN_test.py
+++ Tutorial/Tensorflow/04.LSTM/tensorflow_RNN_test.py
@@ -699,9 +699,8 @@ def test_LSTM_layer2():
 
     sess.close()
 
-
 def test_GRU_layer1():
-    forward_rnn_layers = [tf.nn.rnn_cell.GRUCell(size, activation=tf.keras.activations.linear, dtype=tf.float32 ) for size in [2,2]]#,2]]
+    forward_rnn_layers = [tf.nn.rnn_cell.GRUCell(size, activation=tf.keras.activations.linear, dtype=tf.float32 ) for size in [2]]#,2]]
     #backward_rnn_layers = [tf.nn.rnn_cell.BasicRNNCell(size, activation=tf.keras.activations.linear, dtype=tf.float32) for size in [2,2]]
 
     forward_multi_rnn_cell = tf.nn.rnn_cell.MultiRNNCell(forward_rnn_layers)
@@ -718,8 +717,9 @@ def test_GRU_layer1():
 
 
     config = forward_rnn_layers[0].get_config()
-    config['trainable'] = False
 
+    fw, bw = bi_state
+   
     config1 = forward_rnn_layers[0].get_config()
 
     init_op = tf.global_variables_initializer()
@@ -729,6 +729,18 @@ def test_GRU_layer1():
     sess = tf.Session()
 
     sess.run(init_op)
+
+    print("\n=== Bi state ===")
+    print(bi_state)
+    print("\n=== fw ===")
+    print(fw)
+    print("\n=== fw[0] ===")
+    print(fw[0])
+    print("\n=== bw ===")
+    print(bw)
+    print("\n=== bw[0] ===")
+    print(bw[0])
+
 
     def sigmoid(x):
         return 1/(1 + np.exp(-x))
@@ -744,7 +756,7 @@ def test_GRU_layer1():
     print("\n=== RNN layer 2 ===")
 
     ## LSTM setting
-    gate_w1, gate_b1, candidate_w1, candidate_b1, _,_,_,_  = trainable_vars #fw_rnn_w2, fw_rnn_b2 = trainable_vars #, bw_rnn_w1,  bw_rnn_b1  bw_rnn_w2, bw_rnn_b2 = trainable_vars
+    gate_w1, gate_b1, candidate_w1, candidate_b1 = trainable_vars #fw_rnn_w2, fw_rnn_b2 = trainable_vars #, bw_rnn_w1,  bw_rnn_b1  bw_rnn_w2, bw_rnn_b2 = trainable_vars
 
     print("\n=== gate weight ===")
     print(gate_w1)
@@ -870,20 +882,349 @@ def test_GRU_layer1():
     new_candidate2 = np.add(new_input1_x2.dot(candidate_w1), candidate_b1)
     print("\n=== new_candidate2 in layer1 ===\n{}".format(new_candidate2))
     
-    layer1_h2 = np.add(np.multiply(z2_layer1, layer1_h1), np.multiply((1-z2_layer1), new_candidate2))
+    layer2_h2 = np.add(np.multiply(z2_layer1, layer1_h1), np.multiply((1-z2_layer1), new_candidate2))
 
-    print("\n=== h2 in layer 1 ===\n{}".format(layer1_h2))
+    print("\n=== h2 in layer 2 ===\n{}".format(layer2_h2))
 
 
     print("\n=== bi rnn output ===\n{}".format(sess.run(bi_output)))
 
     print("\n=== bi state output ===\n{}".format(sess.run(bi_state)))
 
-
-
+    print("\n=== config forward rnn cell ===")
     print(config)
-    print(config1)
-  #  print(sess.run(config))
+    print("\n=== fw ===")
+    print(sess.run(fw))
+    print("\n=== fw[0] ===")
+    print(sess.run(fw[0]))
+  
+
+    print("\n=== bw ===")
+    print(sess.run(bw))
+    print("\n=== bw[0] ===")
+    print(sess.run(bw[0]))
+
+
+
+
+
+    sess.close()
+
+
+
+def test_GRU_layer2():
+    forward_rnn_layers = [tf.nn.rnn_cell.GRUCell(size, activation=tf.keras.activations.linear, dtype=tf.float32 ) for size in [2,2]]#,2]]
+    #backward_rnn_layers = [tf.nn.rnn_cell.BasicRNNCell(size, activation=tf.keras.activations.linear, dtype=tf.float32) for size in [2,2]]
+
+    forward_multi_rnn_cell = tf.nn.rnn_cell.MultiRNNCell(forward_rnn_layers)
+    #backward_multi_rnn_cell = tf.nn.rnn_cell.MultiRNNCell(backward_rnn_layers)
+
+    x = tf.constant([[[1,2],[3,4]]], dtype=tf.float32)
+
+    uni_output, uni_state = tf.nn.dynamic_rnn(cell=forward_multi_rnn_cell, sequence_length=[2], inputs=x, dtype=tf.float32)
+    bi_output, bi_state = tf.nn.bidirectional_dynamic_rnn(cell_fw = forward_multi_rnn_cell,
+                                                          cell_bw = forward_multi_rnn_cell,#backward_multi_rnn_cell, 
+                                                          inputs=x,
+                                                          sequence_length=[2],
+                                                          dtype=tf.float32)
+
+
+    config = forward_rnn_layers[0].get_config()
+
+    fw, bw = bi_state
+   
+
+    config1 = forward_rnn_layers[0].get_config()
+
+    init_op = tf.global_variables_initializer()
+
+    tf_global_variables = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
+
+    sess = tf.Session()
+
+    sess.run(init_op)
+
+    print("\n=== Bi state ===")
+    print(bi_state)
+    print("\n=== fw ===")
+    print(fw)
+    print("\n=== fw[0] ===")
+    print(fw[0])
+    print("\n=== fw[1] ===")
+    print(fw[1])
+    print("\n=== bw ===")
+    print(bw)
+    print("\n=== bw[0] ===")
+    print(bw[0])
+    print("\n=== bw[1] ===")
+    print(bw[1])   
+
+
+    def sigmoid(x):
+        return 1/(1 + np.exp(-x))
+
+    print("\n=== Trainable Variables ===")
+    trainable_vars = sess.run(tf_global_variables)
+    print(trainable_vars)
+
+    print("\n=== Trainable Variable name ===")
+    for idx, val in enumerate(tf_global_variables):
+       print("{}-{}: {}".format(idx, val.name, val))
+
+    print("\n=== RNN layer 2 ===")
+
+    ## LSTM setting
+    gate_w1, gate_b1, candidate_w1, candidate_b1, gate_w2, gate_b2, candidate_w2, candidate_b2  = trainable_vars #fw_rnn_w2, fw_rnn_b2 = trainable_vars #, bw_rnn_w1,  bw_rnn_b1  bw_rnn_w2, bw_rnn_b2 = trainable_vars
+
+    print("\n=== gate weight ===")
+    print(gate_w1)
+    print("\n=== gate bias ===")
+    print(gate_b1)
+
+  
+    print("\n=== x ===\n{}".format(sess.run(x)))
+
+    x1 = np.array([1.,2.], dtype=np.float32)
+    print("\n=== x1 ===\n{}".format(x1))
+
+    x2 = np.array([3.,4.], dtype=np.float32)
+    print("\n=== x2 ===\n{}".format(x2))
+
+    h0 = np.array([0., 0.], dtype=np.float32)
+    print("\n===  h0 in layer1 ===\n{}".format(h0))
+
+    input1_x1 = np.concatenate((x1, h0), axis=-1)
+    print("\n=== real input1 x1: concat(x1, h0) === \n{}".format(input1_x1))
+
+    gate1_h1 = sigmoid(np.add(input1_x1.dot(gate_w1), gate_b1))
+    print("\n=== gate1_h1 in layer1 (sigmoid(np.add(input1_x1.dot(gate_w1), gate_b1))) === \n{}".format(gate1_h1))
+
+    r1_layer1 = gate1_h1[0:2]
+
+    z1_layer1 = gate1_h1[2:4]
+
+    reset_h0 = np.multiply(r1_layer1, h0)
+   
+    print("\n=== reset_h0 ===\n{}".format(reset_h0))
+
+    new_input1_x1 = np.concatenate((x1, reset_h0), axis=-1)
+    print("\n=== new_input1_x1 ===\n{}".format(new_input1_x1))
+
+    new_candidate1 = np.add(new_input1_x1.dot(candidate_w1), candidate_b1)
+    print("\n=== new_candidate1 in layer1 ===\n{}".format(new_candidate1))
+    
+    layer1_h1 = np.add(np.multiply(z1_layer1, h0), np.multiply((1-z1_layer1), new_candidate1))
+
+    print("\n=== h1 in layer 1 ===\n{}".format(layer1_h1))
+
+    input1_x2 = np.concatenate((x2, layer1_h1), axis=-1)
+    print("\n=== real input1 x2: concat(x2, laye1_h1) === \n{}".format(input1_x2))
+
+    gate1_h2 = sigmoid(np.add(input1_x2.dot(gate_w1), gate_b1))
+    print("\n=== gate1_h2 in layer1 (sigmoid(np.add(input1_x2.dot(gate_w1), gate_b1))) === \n{}".format(gate1_h2))
+
+    r2_layer1 = gate1_h2[0:2]
+
+    z2_layer1 = gate1_h2[2:4]
+
+    reset_h1 = np.multiply(r2_layer1, layer1_h1)
+   
+    print("\n=== reset_h1 ===\n{}".format(reset_h1))
+
+    new_input1_x2 = np.concatenate((x2, reset_h1), axis=-1)
+    print("\n=== new_input_x2 ===\n{}".format(new_input1_x2))
+
+    new_candidate2 = np.add(new_input1_x2.dot(candidate_w1), candidate_b1)
+    print("\n=== new_candidate2 in layer1 ===\n{}".format(new_candidate2))
+    
+    layer1_h2 = np.add(np.multiply(z2_layer1, layer1_h1), np.multiply((1-z2_layer1), new_candidate2))
+
+    print("\n=== h2 in layer 1 ===\n{}".format(layer1_h2))
+
+    layer2_h0 = h0
+     
+    input2_x1 = np.concatenate((layer1_h1, layer2_h0), axis=-1)
+    print("\n=== real input2 x1: concat(layer1_h1, layer2_h0) === \n{}".format(input2_x1))
+
+    gate2_h1 = sigmoid(np.add(input2_x1.dot(gate_w2), gate_b2))
+    print("\n=== gate2_h1 in layer2 (sigmoid(np.add(input2_x1.dot(gate_w2), gate_b2))) === \n{}".format(gate2_h1))
+
+    r1_layer2 = gate2_h1[0:2]
+
+    z1_layer2 = gate2_h1[2:4]
+
+    reset_h0_layer2 = np.multiply(r1_layer2, layer2_h0)
+   
+    print("\n=== reset_h0_layer2 ===\n{}".format(reset_h0_layer2))
+
+    new_input2_x1 = np.concatenate((layer1_h1, reset_h0_layer2), axis=-1)
+    print("\n=== new_input2_x1 ===\n{}".format(new_input2_x1))
+
+    new_candidate1_layer2 = np.add(new_input2_x1.dot(candidate_w2), candidate_b2)
+    print("\n=== new_candidate1 in layer2 ===\n{}".format(new_candidate1_layer2))
+    
+    layer2_h1 = np.add(np.multiply(z1_layer2, layer2_h0), np.multiply((1-z1_layer2), new_candidate1_layer2))
+
+    print("\n=== h1 in layer 2 ===\n{}".format(layer2_h1))
+
+    input2_x2 = np.concatenate((layer1_h2, layer2_h1), axis=-1)
+    print("\n=== real input2 x2: concat(layer1_h2, laye2_h1) === \n{}".format(input2_x2))
+
+    gate2_h2 = sigmoid(np.add(input2_x2.dot(gate_w2), gate_b2))
+    print("\n=== gate2_h2 in layer2 (sigmoid(np.add(input2_x2.dot(gate_w2), gate_b2))) === \n{}".format(gate2_h2))
+
+    r2_layer2 = gate2_h2[0:2]
+
+    z2_layer2 = gate2_h2[2:4]
+
+    reset_h1_layer2 = np.multiply(r2_layer2, layer2_h1)
+   
+    print("\n=== reset_h1_layer2 ===\n{}".format(reset_h1_layer2))
+
+    new_input2_x2 = np.concatenate((layer1_h2, reset_h1_layer2), axis=-1)
+    print("\n=== new_input2_x2 ===\n{}".format(new_input2_x2))
+
+    new_candidate2_layer2 = np.add(new_input2_x2.dot(candidate_w2), candidate_b2)
+    print("\n=== new_candidate2 in layer2 ===\n{}".format(new_candidate2_layer2))
+    
+    layer2_h2 = np.add(np.multiply(z2_layer2, layer2_h1), np.multiply((1-z2_layer2), new_candidate2_layer2))
+
+    print("\n=== h2 in layer 2 ===\n{}".format(layer2_h2))
+
+
+    print("\n=== rnn output ===\n{}".format(sess.run(uni_output)))
+
+    print("\n=== rnn state ===\n{}".format(sess.run(uni_state)))
+
+    print("\n=== x ===\n{}".format(sess.run(x)))
+
+    x1 = np.array([3.,4.], dtype=np.float32)
+    print("\n=== x1 ===\n{}".format(x1))
+
+    x2 = np.array([1.,2.], dtype=np.float32)
+    print("\n=== x2 ===\n{}".format(x2))
+
+    h0 = np.array([0.,0.], dtype=np.float32)
+    print("\n===  h0 in layer1 ===\n{}".format(h0))
+
+    input1_x1 = np.concatenate((x1, h0), axis=-1)
+    print("\n=== real input1 x1: concat(x1, h0) === \n{}".format(input1_x1))
+
+    gate1_h1 = sigmoid(np.add(input1_x1.dot(gate_w1), gate_b1))
+    print("\n=== gate1_h1 in layer1 (sigmoid(np.add(input1_x1.dot(gate_w1), gate_b1))) === \n{}".format(gate1_h1))
+
+    r1_layer1 = gate1_h1[0:2]
+
+    z1_layer1 = gate1_h1[2:4]
+
+    reset_h0 = np.multiply(r1_layer1, h0)
+   
+    print("\n=== reset_h0 ===\n{}".format(reset_h0))
+
+    new_input1_x1 = np.concatenate((x1, reset_h0), axis=-1)
+    print("\n=== new_input_x1 ===\n{}".format(new_input1_x1))
+
+    new_candidate1 = np.add(new_input1_x1.dot(candidate_w1), candidate_b1)
+    print("\n=== new_candidate1 in layer1 ===\n{}".format(new_candidate1))
+    
+    layer1_h1 = np.add(np.multiply(z1_layer1, h0), np.multiply((1-z1_layer1), new_candidate1))
+
+    print("\n=== h1 in layer 1 ===\n{}".format(layer1_h1))
+
+    input1_x2 = np.concatenate((x2, layer1_h1), axis=-1)
+    print("\n=== real input1 x2: concat(x2, laye1_h1) === \n{}".format(input1_x2))
+
+    gate1_h2 = sigmoid(np.add(input1_x2.dot(gate_w1), gate_b1))
+    print("\n=== gate1_h2 in layer1 (sigmoid(np.add(input1_x2.dot(gate_w1), gate_b1))) === \n{}".format(gate1_h2))
+
+    r2_layer1 = gate1_h2[0:2]
+
+    z2_layer1 = gate1_h2[2:4]
+
+    reset_h1 = np.multiply(r2_layer1, layer1_h1)
+   
+    print("\n=== reset_h1 ===\n{}".format(reset_h1))
+
+    new_input1_x2 = np.concatenate((x2, reset_h1), axis=-1)
+    print("\n=== new_input_x2 ===\n{}".format(new_input1_x2))
+
+    new_candidate2 = np.add(new_input1_x2.dot(candidate_w1), candidate_b1)
+    print("\n=== new_candidate2 in layer1 ===\n{}".format(new_candidate2))
+    
+    layer1_h2 = np.add(np.multiply(z2_layer1, layer1_h1), np.multiply((1-z2_layer1), new_candidate2))
+
+    print("\n=== h2 in layer 1 ===\n{}".format(layer1_h2))
+
+    layer2_h0 = h0
+     
+    input2_x1 = np.concatenate((layer1_h1, layer2_h0), axis=-1)
+    print("\n=== real input2 x1: concat(layer1_h1, layer2_h0) === \n{}".format(input2_x1))
+
+    gate2_h1 = sigmoid(np.add(input2_x1.dot(gate_w2), gate_b2))
+    print("\n=== gate2_h1 in layer2 (sigmoid(np.add(input2_x1.dot(gate_w2), gate_b2))) === \n{}".format(gate2_h1))
+
+    r1_layer2 = gate2_h1[0:2]
+
+    z1_layer2 = gate2_h1[2:4]
+
+    reset_h0_layer2 = np.multiply(r1_layer2, layer2_h0)
+   
+    print("\n=== reset_h0_layer2 ===\n{}".format(reset_h0_layer2))
+
+    new_input2_x1 = np.concatenate((layer1_h1, reset_h0_layer2), axis=-1)
+    print("\n=== new_input2_x1 ===\n{}".format(new_input2_x1))
+
+    new_candidate1_layer2 = np.add(new_input2_x1.dot(candidate_w2), candidate_b2)
+    print("\n=== new_candidate1 in layer2 ===\n{}".format(new_candidate1_layer2))
+    
+    layer2_h1 = np.add(np.multiply(z1_layer2, layer2_h0), np.multiply((1-z1_layer2), new_candidate1_layer2))
+
+    print("\n=== h1 in layer 2 ===\n{}".format(layer2_h1))
+
+    input2_x2 = np.concatenate((layer1_h2, layer2_h1), axis=-1)
+    print("\n=== real input2 x2: concat(layer1_h2, laye2_h1) === \n{}".format(input2_x2))
+
+    gate2_h2 = sigmoid(np.add(input2_x2.dot(gate_w2), gate_b2))
+    print("\n=== gate2_h2 in layer2 (sigmoid(np.add(input2_x2.dot(gate_w2), gate_b2))) === \n{}".format(gate2_h2))
+
+    r2_layer2 = gate2_h2[0:2]
+
+    z2_layer2 = gate2_h2[2:4]
+
+    reset_h1_layer2 = np.multiply(r2_layer2, layer2_h1)
+   
+    print("\n=== reset_h1_layer2 ===\n{}".format(reset_h1_layer2))
+
+    new_input2_x2 = np.concatenate((layer1_h2, reset_h1_layer2), axis=-1)
+    print("\n=== new_input2_x2 ===\n{}".format(new_input2_x2))
+
+    new_candidate2_layer2 = np.add(new_input2_x2.dot(candidate_w2), candidate_b2)
+    print("\n=== new_candidate2 in layer2 ===\n{}".format(new_candidate2_layer2))
+    
+    layer2_h2 = np.add(np.multiply(z2_layer2, layer2_h1), np.multiply((1-z2_layer2), new_candidate2_layer2))
+
+    print("\n=== h2 in layer 2 ===\n{}".format(layer2_h2))
+
+
+    print("\n=== bi rnn output ===\n{}".format(sess.run(bi_output)))
+
+    print("\n=== bi state output ===\n{}".format(sess.run(bi_state)))
+
+    print("\n=== config forward rnn cell ===")
+    print(config)
+    print("\n=== fw ===")
+    print(sess.run(fw))
+    print("\n=== fw[0] ===")
+    print(sess.run(fw[0]))
+    print("\n=== fw[1] ===")
+    print(sess.run(fw[1]))
+    print("\n=== bw ===")
+    print(sess.run(bw))
+    print("\n=== bw[0] ===")
+    print(sess.run(bw[0]))
+    print("\n=== bw[1] ===")
+    print(sess.run(bw[1]))   
+
     sess.close()
 
 
@@ -895,6 +1236,8 @@ if __name__ == "__main__":
 
    #test_LSTM_layer1()
 
-   test_LSTM_layer2()
+   #test_LSTM_layer2()
    
    #test_GRU_layer1()
+   
+   test_GRU_layer2()
